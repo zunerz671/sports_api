6551c2b96655259360e86d4214e6b1b7d89f5b37
zunerz671 <zunerz671@gmail.com>
2025-09-30 19:09:18 +1000

    added sports controller and routes

---
+++ app/controllers/sports_controller.rb
@@ -0,0 +1,19 @@
+class SportsController < ApplicationController
+  def index
+    sports = Sport.all
+
+    render json: sports
+  end
+
+  def create
+    sport = Sport.new(
+      name: params[:name],
+      players: params[:players]
+    )
+    if sport.save
+      render json: { message: "Sport created successfully" }, status: :created
+    else
+      render json: { errors: sport.errors.full_message }, status: :unprocessable_entity
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "/sports" => "sports#index"
+  post "/sports" => "sports#create"
 end
